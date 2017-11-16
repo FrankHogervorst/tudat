f1468616167632f845b38d924b70c01d3561771b
Frank Hogervorst <f.c.hogervorst@student.tudelft.nl>
2017-11-16 12:30:17 +0100

    removed output of dependent variables to save

--- Tudat/SimulationSetup/PropagationSetup/dynamicsSimulator.h
+++ Tudat/SimulationSetup/PropagationSetup/dynamicsSimulator.h
@@ -206,10 +206,10 @@ public:
 
             if( propagatorSettings_->getDependentVariablesToSave( )->printDependentVariableTypes_ )
             {
-                std::cout<<"Dependent variables being saved, output vectors contain: "<<std::endl<<
-                           "Vector entry, Vector contents"<<std::endl;
-                utilities::printMapContents(
-                            dependentVariableIds_ );
+//                std::cout<<"Dependent variables being saved, output vectors contain: "<<std::endl<<
+//                           "Vector entry, Vector contents"<<std::endl;
+//                utilities::printMapContents(
+//                            dependentVariableIds_ );
             }
         }
 
